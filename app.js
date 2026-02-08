3f3e03da26aaddba7fccd0e345a7b7667413d52c
Sudhavani Pishike <sudhavani.pishike@phenom.com>
2026-02-08 17:25:05 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,18 @@
+//   const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello world from React! ");
+//    const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(heading);
+
+const parent = React.createElement(
+              "div",
+              {id:"parent"}, 
+              React.createElement(
+                "div",
+                 {id:"child"},
+                React.createElement("h1", {}, "I'm an h1 tag")
+              )  
+           
+);
+
+console.log(parent);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
